4805b7cd6e5859dcac618cfdbef5724b8c65f26d
Cheton Wu <cheton@gmail.com>
2018-01-18 17:34:14 +0800

    Setup tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import { test } from 'tap';
+import '../setupTests';
+import { Nav, NavItem } from '../src';
 
-test('noop', (t) => {
+test('<Nav />', (t) => {
+    const wrapper = mount((
+        <Nav
+            navStyle="tabs"
+            activeKey={1}
+            onSelect={() => {
+                // TODO
+            }}
+        >
+            <NavItem eventKey={1}>NavItem 1</NavItem>
+            <NavItem eventKey={2}>NavItem 2</NavItem>
+            <NavItem eventKey={3}>NavItem 3</NavItem>
+        </Nav>
+    ));
+    t.equal(wrapper.find(Nav).length, 1, 'should render <Nav /> component');
     t.end();
 });
