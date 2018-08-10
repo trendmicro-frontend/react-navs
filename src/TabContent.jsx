6496d1b1dcc73dfd4f3f5e689439f32f92628632
Cheton Wu <cheton@gmail.com>
2018-08-10 17:04:23 +0800

    Address an issue where checking element type for imported components is not possible with projects using react-hot-loader

--- src/TabContent.jsx
+++ src/TabContent.jsx
@@ -4,6 +4,8 @@ import React, { cloneElement, PureComponent } from 'react';
 import TabPane from './TabPane';
 import styles from './index.styl';
 
+const getComponentType = (Component) => (Component ? (<Component />).type : undefined);
+
 class TabContent extends PureComponent {
     static propTypes = {
         activeKey: PropTypes.any
@@ -18,7 +20,7 @@ class TabContent extends PureComponent {
                 className={cx(className, styles.tabContent)}
             >
                 {React.Children.map(children, child => {
-                    if (React.isValidElement(child) && child.type === TabPane) {
+                    if (React.isValidElement(child) && child.type === getComponentType(TabPane)) {
                         const active = (child.props.eventKey === activeKey);
 
                         return cloneElement(child, {
